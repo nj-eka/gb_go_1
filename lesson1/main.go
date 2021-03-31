660315bfa6e8636188167d5d7fea65d15c38d3be
nj-eka <njeka0108@gmail.com>
2021-03-31 13:08:40 +0300

    add make

--- lesson1/main.go
+++ lesson1/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/beevik/ntp"
 	"github.com/nj-eka/gb_go_1/lesson1/greeting"
+	"github.com/nj-eka/gb_go_1/lesson1/version"
 )
 
 //https://www.pool.ntp.org/zone/ru
@@ -14,24 +15,23 @@ const ntpMSKHost = "ntp1.stratum1.ru"
 const ntpUTCHost = "0.ru.pool.ntp.org"
 
 func main() {
-	fmt.Println("Привет! Сверим часы:")
-	fmt.Println("Местное время: ", time.Now())
+	fmt.Printf("Привет! Это версия [%s] коммита [%s] от [%s]\n", version.Version, version.Commit, version.BuildTime)
+	fmt.Println("Сверим наши часы: \n	- местное время: ", time.Now())
 	ntpTime, err := ntp.Time(ntpMSKHost)
 	if err != nil {
 		log.Fatalf("Error: %s", err)
 	} else {
-		fmt.Println("Московское время: ", ntpTime)
+		fmt.Println("	- Московское время: ", ntpTime)
 	}
 	response, err := ntp.Query(ntpUTCHost)
 	if err != nil {
 		log.Fatalf("Error: %s", err)
 	} else {
-		fmt.Printf("В Гринвиче: %v\nС уточнениями: %v\n", response.Time, time.Now().Add(response.ClockOffset))
+		fmt.Printf("	- в Гринвиче: %v\n	- с поправкой: %v\n", response.Time, time.Now().Add(response.ClockOffset))
 	}
 	err = response.Validate()
 	if err == nil {
 		fmt.Println("Кстати, возможна синхронизация.")
 	}
 	fmt.Println(greeting.SayGo())
-
 }
