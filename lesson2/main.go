977e736924af2233e23e7db06f28bebb628271f8
nj-eka <njeka0108@gmail.com>
2021-04-05 20:16:51 +0300

    lesson2/v0.0.1

---
+++ lesson2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nj-eka/gb_go_1/lesson1/measure"
+)
+
+func main() {
+	fmt.Println("Select: [1, 2, 3]")
+	fmt.Println("1 - Rectangle Area.")
+	fmt.Println("2 - Circle D L")
+	fmt.Println("3 - Number Parts")
+
+	var choice string
+	fmt.Scan(&choice)
+	switch choice {
+	case "1":
+		measure.RectangleS()
+	case "2":
+		measure.CircleDL()
+	case "3":
+		measure.NumberParts()
+	default:
+		fmt.Println("Unknown operation")
+	}
+
+}
