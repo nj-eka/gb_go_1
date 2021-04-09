6eb2ec2dc02ee472ce352356f7a9856028eefadc
nj-eka <njeka0108@gmail.com>
2021-04-09 20:51:33 +0300

    lesson2 add primes

--- lesson3/go.mod
+++ lesson3/go.mod
@@ -2,4 +2,5 @@ module github.com/nj-eka/gb_go_1/lesson3
 
 go 1.16
 
-//replace github.com/nj-eka/gb_go_1/lesson3/calclib => ./calclib
+replace github.com/nj-eka/gb_go_1/lesson3/calc => ./calc
+replace github.com/nj-eka/gb_go_1/lesson3/prime => ./prime
--- lesson3/main.go
+++ lesson3/main.go
@@ -6,21 +6,43 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/nj-eka/gb_go_1/lesson3/calclib"
+	calc "github.com/nj-eka/gb_go_1/lesson3/calc"
+	prime "github.com/nj-eka/gb_go_1/lesson3/prime"
 )
 
 func main() {
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("calc: ")
-		input, _ := reader.ReadString('\n')
-		lexer := calclib.Lexer(input)
-		interpreter := calclib.Interpreter(&lexer)
-		result, err := interpreter.Result()
-		if err == nil {
-			fmt.Printf("res: %s\n", strconv.FormatFloat(result, 'f', -1, 64))
-		} else {
-			fmt.Printf("%s\n", err)
+	fmt.Println("Select: [1, 2]")
+	fmt.Println("1 - simple calculator")
+	fmt.Println("2 - primes")
+
+	var choice string
+	fmt.Scan(&choice)
+	switch choice {
+	case "1":
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print("calc: ")
+			input, _ := reader.ReadString('\n')
+			lexer := calc.Lexer(input)
+			interpreter := calc.Interpreter(&lexer)
+			result, err := interpreter.Result()
+			if err == nil {
+				fmt.Printf("res: %s\n", strconv.FormatFloat(result, 'f', -1, 64))
+			} else {
+				fmt.Printf("%s\n", err)
+			}
 		}
+	case "2":
+		fmt.Println("Input int number up to which you want to find primes: ")
+		var n int
+		if _, err := fmt.Scanf("%d", &n); err == nil {
+			primes := prime.FindPrimesByBruteForce(n)
+			fmt.Println(primes)
+			for _, p := range primes {
+				fmt.Println(p)
+			}
+		}
+	default:
+		fmt.Println("Unknown operation")
 	}
 }
