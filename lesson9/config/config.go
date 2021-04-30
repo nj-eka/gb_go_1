fff1716f4694a16a8b67371dc25cd4f7a2ee43b8
nj-eka <njeka0108@gmail.com>
2021-04-30 22:33:56 +0300

    lesson9/v0.0.1

--- lesson9/config/config.go
+++ lesson9/config/config.go
@@ -13,27 +13,27 @@ import (
 
 type Config struct {
 	Server struct {
-		Host string `envconfig:"SERVER_HOST", yaml:"host", json:"host"`
-		Port string `envconfig:"SERVER_PORT", yaml:"port", json:"port"`
-		Path string `envconfig:"SERVER_PATH", yaml:"path", json:"path"`
+		Host    string  `envconfig:"SERVER_HOST", yaml:"host", json:"host"`
+		Port    int     `envconfig:"SERVER_PORT", yaml:"port", json:"port"`
+		Path    string  `envconfig:"SERVER_PATH", yaml:"path", json:"path"`
+		Timeout float32 `envconfig:"SERVER_TIMEOUT", yaml:"timeout", json:"timeout"`
+		Check   bool    `envconfig:"SERVER_CHECK", yaml:"check", json:"check"`
 	} `yaml:"server" json:"server"`
 }
 
 func processError(err error) {
 	if err != nil {
 		log.Fatal(err)
-		// fmt.Println(err)
-		// os.Exit(2)
 	}
 }
 
 func readFile(cfg *Config, configPath string) {
-
+	if configPath == "" {
+		return
+	}
 	b, err := ioutil.ReadFile(configPath)
 	processError(err)
-
 	configType := strings.ToLower(path.Ext(configPath))[1:]
-
 	switch configType {
 	case "yaml", "yml":
 		err = yaml.Unmarshal(b, &cfg)
--- lesson9/main.go
+++ lesson9/main.go
@@ -12,8 +12,12 @@ func main() {
 	os.Setenv("SERVER_HOST", "http://geekbrains.com")
 	os.Setenv("SERVER_PORT", "80")
 	os.Setenv("SERVER_PATH", "/index.html")
+	os.Setenv("SERVER_TIMEOUT", "0")
+	os.Setenv("SERVER_CHECK", "True")
 
-	configPath := flag.String("config", "config.yaml", "Config file path")
+	configPath := flag.String("config", "", "Config file path")
+	//	host := flag.String("host", "http://eka", "Set host")
+
 	flag.Parse()
 	cfg := config.GetConfig(*configPath)
 	fmt.Println(*cfg)
